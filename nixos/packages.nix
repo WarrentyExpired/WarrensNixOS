a5f7c297a621d1a8269e8dc568d79e7a1c2024b6
WarrentyExpired <warrentyexpired@gmail.com>
2025-07-22 19:15:57 -0400

    # Initial Commit Using Flakes

---
+++ nixos/packages.nix
@@ -0,0 +1,73 @@
+{ config, pkgs, inputs, ... }:
+{
+  ### Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+  environment.systemPackages = with pkgs; [
+    ### System Apps ###
+    wget
+    unzip
+    git
+    gh
+    gparted
+    pavucontrol
+    libnotify
+    unrar
+    btop
+    ### WM Apps
+    swaylock-effects
+    waybar
+    swaybg
+    swayidle
+    xwayland-satellite
+    terminator
+    wlogout
+    mako
+    tofi
+    wl-clipboard
+    slurp
+    swappy
+    grim
+    xdg-desktop-portal-gtk
+    xdg-desktop-portal-gnome
+    gnome-keyring
+    nautilus
+
+    ### User Apps ###
+    discord
+    helix
+    zoxide
+    ncdu
+    helix
+    pokeget-rs
+    fastfetch
+    eza
+    wezterm
+
+   #### Software to try out ####
+   nushell
+   mpv
+   vscode-fhs
+
+   ### Stuff for Yazi Terminal File Manager
+   yazi
+   ffmpeg
+   p7zip
+   jq
+   poppler
+   fd
+   ripgrep
+   fzf
+   resvg
+   imagemagick
+
+   ### Themes and fonts###
+   nordic
+   cascadia-code
+   nerd-fonts.caskaydia-cove
+   font-awesome
+  ];
+  ### Programs ###
+  programs.niri.enable = true;
+  programs.firefox.enable = true;
+### End ###
+}
